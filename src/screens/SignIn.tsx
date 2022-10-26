0912e3bdf8fe7421928b429ba7ab5201ee7d1aab
Thiago <thiagocontellid@gmail.com>
2022-10-26 15:00:53 -0300

    Component Library - Componente Heading

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Center, Text } from 'native-base';
+import { VStack, Image, Center, Text, Heading } from 'native-base';
 import BackgroundImg from '@assets/background.png';
 import LogoSvg from '@assets/logo.svg';
 
@@ -14,9 +14,20 @@ export function SignIn() {
 				resizeMode='contain'
 				position='absolute'
 			/>
+
 			<Center my={24}>
 				<LogoSvg />
 				<Text color='gray.100'>Treine sua mente e seu corpo</Text>
+			</Center>
+
+			<Center>
+				<Heading
+					color='gray.100'
+					mb={6}
+					fontSize='xl'
+				>
+					Acesse sua conta
+				</Heading>
 			</Center>
 		</VStack>
 	);
